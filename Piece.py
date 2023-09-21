e7ec12708c2360c3ddf9d86c130f914bbc7b6ff3
Sebastián Maluk <80332200+SebastianMaluk@users.noreply.github.com>
2023-09-20 21:13:20 -0300

    delete home_row attribute

--- Piece.py
+++ Piece.py
@@ -2,6 +2,5 @@ class Piece:
     def __init__(self, id, color, start_position):
         self.id = f"{id}_{color}"
         self.position = start_position
-        self.home_row = False
         self.crown_members: set[Piece] = set()
         self.start_position = start_position
